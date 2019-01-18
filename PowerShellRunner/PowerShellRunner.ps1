cd44ba145073af92669a47776b78ba222db1ae1d
amenprog <amenprog@gmail.com>
2019-01-18 18:04:31 +0200

    Initial Commit

---
+++ PowerShellRunner/PowerShellRunner.ps1
@@ -0,0 +1,10 @@
+﻿write-host "Args.Count: "$args.Length
+if($args.Length -gt 0) {
+	$numOfArgs = $args.Length
+	for ($i=0; $i -le $numOfArgs; $i++)
+	{
+	   write-host $args[$i]
+	}
+}
+write-host "Hit enter to exit..." -ForegroundColor Yellow
+$i = read-host
---
+++ PowerShellRunner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PowerShellRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentFilename = Process.GetCurrentProcess().MainModule.FileName;
+            var psFile = currentFilename.Substring(0, currentFilename.Length - 4) + ".ps1";
+
+            var fileInfo = new FileInfo(psFile);
+
+            var argsText = args.Length > 0 ? (" " + args.Select(m => "" + m + "").Aggregate((m1, m2) => m1 + " " + m2)) : string.Empty;
+
+            if (File.Exists(psFile))
+            {
+                Process.Start("powershell.exe", $@"-ExecutionPolicy Bypass -File {psFile}{argsText}");
+            }
+            else
+            {
+                Console.WriteLine($"{fileInfo.Name} not found");
+            }
+        }
+    }
+}
